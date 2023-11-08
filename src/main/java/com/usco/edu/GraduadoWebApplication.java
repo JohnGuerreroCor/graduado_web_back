6e9374c7e79de1b790538c23c0c5b8d9c914a381
JohnGuerreroCor <john.guerrero@usco.edu.co>
2023-11-08 17:40:24 -0500

    Datos personales front

--- src/main/java/com/usco/edu/GraduadoWebApplication.java
+++ src/main/java/com/usco/edu/GraduadoWebApplication.java
@@ -7,10 +7,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
-public class CarnetUsuarioApplication {
+public class GraduadoWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CarnetUsuarioApplication.class, args);
+		SpringApplication.run(GraduadoWebApplication.class, args);
 	}
 	
 
